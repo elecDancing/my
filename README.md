c9c96839662bdbe61081664138ad19608fe70328
xpzhang <xp17126@163.com>
2023-05-02 11:48:22 +0800

    first commit

--- README.md
+++ README.md
@@ -4,12 +4,12 @@
  * @Author: xp.Zhang
  * @Date: 2023-04-30 20:34:01
  * @LastEditors: xp.Zhang
- * @LastEditTime: 2023-04-30 20:47:18
+ * @LastEditTime: 2023-05-01 10:10:21
 -->
 
 ## 修改的代码上传过程
 
-![image-20230430203946470](./2023-04-30%2020-43-32%20%E7%9A%84%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE.png)
+![image-20230430203946470](./photos/2023-04-30%2020-43-32%20%E7%9A%84%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE.png)
 
 一共有四个仓库，前两个是本地仓库
 
@@ -18,4 +18,37 @@
 ```
 git push origin MY_modified:MY_modified
 ```
+
+## 编译成功后，首次运行
+
+可执行文件   词袋    相机配置文件    数据集位置        TUM数据集对齐文件
+
+```
+./Examples/RGB-D/rgbd_tum ./Vocabulary/ORBvoc.txt ./Examples/RGB-D/TUM1.yaml /home/juo/TUM/rgbd_dataset_freiburg1_desk ./Examples/RGB-D/associations/fr1_desk.txt
+```
+
+### 报错 没有将VTK 加载到环境变量中
+
+![image-20230430203946471](./photos/2.png)
+
+```
+gedit ~/.bashrc
+```
+
+在最后加入
+
+```
+# 设置vtkRendering的目录
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+```
+
+保存后更新环境变量
+
+```
+source ~/.bashrc
+```
+
+运行结果如下图所示
+
+<img src="./photos/3.png" style="zoom: 100%;" />
 
---
+++ build/CMakeFiles/ORB_SLAM3.dir/DependInfo.cmake
@@ -0,0 +1,71 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/juo/MY_orb/src/Atlas.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/Atlas.cc.o"
+  "/home/juo/MY_orb/src/CameraModels/KannalaBrandt8.cpp" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/CameraModels/KannalaBrandt8.cpp.o"
+  "/home/juo/MY_orb/src/CameraModels/Pinhole.cpp" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/CameraModels/Pinhole.cpp.o"
+  "/home/juo/MY_orb/src/Config.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/Config.cc.o"
+  "/home/juo/MY_orb/src/Converter.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/Converter.cc.o"
+  "/home/juo/MY_orb/src/Frame.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/Frame.cc.o"
+  "/home/juo/MY_orb/src/FrameDrawer.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/FrameDrawer.cc.o"
+  "/home/juo/MY_orb/src/G2oTypes.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/G2oTypes.cc.o"
+  "/home/juo/MY_orb/src/GeometricTools.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/GeometricTools.cc.o"
+  "/home/juo/MY_orb/src/ImuTypes.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/ImuTypes.cc.o"
+  "/home/juo/MY_orb/src/KeyFrame.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/KeyFrame.cc.o"
+  "/home/juo/MY_orb/src/KeyFrameDatabase.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/KeyFrameDatabase.cc.o"
+  "/home/juo/MY_orb/src/LocalMapping.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/LocalMapping.cc.o"
+  "/home/juo/MY_orb/src/LoopClosing.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/LoopClosing.cc.o"
+  "/home/juo/MY_orb/src/MLPnPsolver.cpp" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/MLPnPsolver.cpp.o"
+  "/home/juo/MY_orb/src/Map.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/Map.cc.o"
+  "/home/juo/MY_orb/src/MapDrawer.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/MapDrawer.cc.o"
+  "/home/juo/MY_orb/src/MapPoint.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/MapPoint.cc.o"
+  "/home/juo/MY_orb/src/ORBextractor.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/ORBextractor.cc.o"
+  "/home/juo/MY_orb/src/ORBmatcher.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/ORBmatcher.cc.o"
+  "/home/juo/MY_orb/src/OptimizableTypes.cpp" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/OptimizableTypes.cpp.o"
+  "/home/juo/MY_orb/src/Optimizer.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/Optimizer.cc.o"
+  "/home/juo/MY_orb/src/PointCloudMapping.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/PointCloudMapping.cc.o"
+  "/home/juo/MY_orb/src/Settings.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/Settings.cc.o"
+  "/home/juo/MY_orb/src/Sim3Solver.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/Sim3Solver.cc.o"
+  "/home/juo/MY_orb/src/System.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/System.cc.o"
+  "/home/juo/MY_orb/src/Tracking.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/Tracking.cc.o"
+  "/home/juo/MY_orb/src/TwoViewReconstruction.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/TwoViewReconstruction.cc.o"
+  "/home/juo/MY_orb/src/Viewer.cc" "/home/juo/MY_orb/build/CMakeFiles/ORB_SLAM3.dir/src/Viewer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "qh_QHpointer"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL2)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/vtk-8.2"
+  "../"
+  "../include"
+  "../include/CameraModels"
+  "../Thirdparty/Sophus"
+  "/usr/include/eigen3"
+  "/usr/local/lib/cmake/Pangolin/../../../include"
+  "/usr/local/include/pcl-1.12"
+  "/usr/include/ni"
+  "/usr/include/openni2"
+  "../ORB_SLAM3"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  "/opt/ros/melodic/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
